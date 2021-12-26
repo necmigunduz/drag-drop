295e838730a2c0bda2f12089556472dee59a5b7b
necmigunduz <necmigunduz@gmail.com>
2021-12-26 10:46:40 +0300

    Add client t structure

---
+++ rastM/client/src/App.js
@@ -0,0 +1,11 @@
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
+
+function App() {
+  return (
+    <div className="App" stle={{display:'flex', justifyContent:'center', height:'100%'}}>
+      <p>Hello</p>
+    </div>
+  );
+}
+
+export default App;
